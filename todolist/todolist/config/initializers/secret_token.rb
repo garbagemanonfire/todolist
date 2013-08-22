acc0ec53b33dc51821ebdfc73299dd4faeaecf5d
Niles McGvier <nilesvm@gmail.com>
2013-08-22 08:50:13 -0700

    This is my simple to do list's first commit

---
+++ todolist/todolist/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todolist::Application.config.secret_token = '95411f8b5a636b461f051887cb0b45d74d6c0e9b8affe4f4eb5a632f2f81fdda7d2f4ce3fc31c155579abb87ebfbfc2ac4157c16d8961f31301b1f61291ffdc7'
